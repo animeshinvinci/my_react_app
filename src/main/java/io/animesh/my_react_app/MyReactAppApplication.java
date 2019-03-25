9645c197c2fc7cf4af812e51b645b089328aa22f
animesh <animesh@animesh.me>
2019-03-25 23:48:11 +0530

    initail react & spring

---
+++ src/main/java/io/animesh/my_react_app/MyReactAppApplication.java
@@ -0,0 +1,13 @@
+package io.animesh.my_react_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReactAppApplication.class, args);
+	}
+
+}
